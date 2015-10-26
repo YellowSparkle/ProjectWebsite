78a14b2fc83fc7640afb7cb2cb142ced834595eb
Hanze Hogeschool <joostholwerda@hotmail.com>
2015-10-26 14:48:48 +0100

    .

--- include.php
+++ include.php
@@ -1,13 +1,14 @@
 <?php
-$phpusername = "root";
-$phppassword = "";
-$hostname    = "localhost";
-$dbnaam      = "projectdb";
+$phpusername = "root"; 		//gebruikersnaam
+$phppassword = "";			//geen wachtwoord
+$hostname    = "localhost";	//dit connect naar 'localhost' (phpMyadmin)
+$dbnaam      = "projectdb"; //dit is de databasenaam
+// dit gecombineerd heeft alle info om de connectie te maken.
 
-$dbconnect = mysqli_connect($hostname, $phpusername, $phppassword, $dbnaam);
+$dbconnect = mysqli_connect($hostname, $phpusername, $phppassword, $dbnaam); //de connectie met de database
 
 if (mysqli_connect_errno()) { /* Check connection */
 	echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
-}
+}//dit geeft een error als hij niet kan connecten
 
 ?>
