941846202685f16ba420f6fe3a3ea45c6b37a28d
Hanze Hogeschool <joostholwerda@hotmail.com>
2015-10-26 16:52:06 +0100

    localhost aangepast. inloggegevens toegevoegd

--- include.php
+++ include.php
@@ -12,7 +12,7 @@ if (mysqli_connect_errno()) { /* Check connection
 	echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
 }*/
 
-	$link = mysql_connect("localhost", "root", "")
+	$link = mysql_connect("http://yellowsparkle.sentientturtle.me/phpmyadmin/localhost", "Joost", "Project")
 		or die("Error connecting to server");
 	
 	mysql_select_db("projectdb")
