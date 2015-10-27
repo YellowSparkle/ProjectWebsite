2a13d2783c55fd343fc5ae9bdca1dbb9cf7600f8
Hanze Hogeschool <ivo1119@live.nl>
2015-10-27 14:07:25 +0100

    changes lel

--- Buy/product_buy_page.php
+++ Buy/product_buy_page.php
@@ -1,40 +1,43 @@
 <?php
    	require "../utility/HTMLGenerator.php";
 	require "../utility/Header.php";
+	
 	generateTitle("Order complete");
 	generateHeader();
+	
+	
 ?>
 
 
-<!DOCTYPE html>
 
 <html lang="en">
 	<head>
 		<title>Offer your product</title>
+		
 	</head>
 
 	<body>
-		
-<div class="absolute_buy_page">
+		<?php require_once '../utility/stylesheet.css';?>
+<div class="buy_page">
   	<ul class="checkbox">
     <li><input type="checkbox" name="delivery" id="del1" value="in" /><label for="del1">Dine in  </label></li>
     <li><input type="checkbox" name="delivery" id="del2" value="out" /><label for="del2">Carry out  </label></li>
     <li><input type="checkbox" name="delivery" id="del3" value="delivery" /><label for="del3">Delivery  </label></li>
   	</ul>
 </div>
+<style>/* hier staan buy layouts*/
 
+	div.buy_page 
+		{
+    		position: absolute;
+    		top: 35%;
+    		left: 20%;
+    		width: 200px;
+    		height: 100px;
+    		border: 3px solid green;
+    	}
+
+/* eind buy layout*/</style>
 	</body>
 	
-	<style type="text/css">
-	div.absolute_buy_page 
-	{
-    	position: absolute;
-    	top: 35%;
-    	left: 20%;
-    	width: 200px;
-    	height: 100px;
-    	border: 3px solid green;
-    	
-	}
-</style>
 </html>
