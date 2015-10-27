016aefa52e82fd557efe4518b62ac471a3e2ae06
Hanze Hogeschool <xavier.kerkmeester@gmail.com>
2015-10-27 14:07:15 +0100

    Header change

--- utility/Header.php
+++ utility/Header.php
@@ -10,3 +10,18 @@
 	}
 
 ?>
+
+<link rel="stylesheet" type="text/css" href="../utility/stylesheet.css">
+
+
+<div class=headlogin>
+	<form method="post" action="../utility/Login.php">
+		<table>
+        <tr>
+       		<td><input type="text" name="login" value="" placeholder="Email"></td>
+        	<td><input type="password" name="password" value="" placeholder="Password"></td>        
+        </tr>
+        </table>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+      </form>
+   </div>
