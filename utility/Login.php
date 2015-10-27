5795e4048a2e7044bed13e9f93b5e99b4f5942c3
Hanze Hogeschool <onzin09@gmail.com>
2015-10-27 12:06:55 +0100

    De login werkt bijna, met hash nog niet, zonder hash kan je inloggen

--- utility/Login.php
+++ utility/Login.php
@@ -1,12 +1,52 @@
 <?php
 	require_once "../utility/HTMLGenerator.php";
 	require_once "../utility/Header.php";
+	include '../include.php';
 	
-	if (array_key_exists("login", $_POST) && (array_key_exists("pass", $_POST))){
+	if (array_key_exists("login", $_POST) && (array_key_exists("password", $_POST))){
 		$username = $_POST["login"];
-		$password = $_POST["pass"];
+		$password = $_POST["password"];}
 		
 		// TODO: Database magic here.
+		
+		
+		$sql=" 
+		SELECT * 
+		FROM Customer 
+		WHERE Email='".$username."' ";
+		$result=mysql_query($sql);		
+		/*if ($sql==TRUE) {
+			$result1=1;
+		}
+		if ($sql==FALSE){
+			$result1=0;
+		}*/
+		var_dump($result);
+		$count=mysql_num_rows($result);
+		if($count==1){
+			$row = mysql_fetch_assoc($result);
+			if ($password == $row['Password']){ 
+        	echo "Login Successful";
+			 
+			 
+			 /*password_verify('$_POST(login)', $row['Password'])){
+				session_register("username");
+				session_register("password");
+				echo "Login Successful";*/
+			} else {
+				echo "Login Unsuccesful! 0_0";
+			}
+		}
+
+		
+		
+/*		if (password_verify('$_POST(login)', '$hash')) {
+	    echo 'Password is valid!';
+		} else {
+	    echo 'Invalid password.';
+		}
+		
+		
 		
 		// TODO: Session magic here.
 		
@@ -17,5 +57,6 @@
 	generateHeader();
 	echo "Logging in please wait...<br>";
 	echo "<a href='../index/homepage.php'>If nothing happens after 5 seconds, click here to go back.</a>";
-	generateFoot();
+	generateFoot();*/
+	
 ?>
