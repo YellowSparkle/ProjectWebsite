3e12cdd15fcf1e7fb386bdab802b53d30cca5afa
Danny <danny.oldenburger@gmail.com>
2015-10-29 15:02:28 +0100

    Login work.

--- utility/Login.php
+++ utility/Login.php
@@ -2,8 +2,6 @@
 	require_once "../utility/HTMLGenerator.php";
 	require_once "../utility/Header.php";
 	include '../include.php';
-	generateTitle("Logging in...");
-	//generateTitle("Logging in...", "../index/homepage.php");
 	if (array_key_exists("login", $_POST) && (array_key_exists("password", $_POST))){
 		$username = $_POST["login"];
 		$password = $_POST["password"];
@@ -16,15 +14,14 @@
 		$count=mysql_num_rows($result);
 		if($count==1){
 			$row = mysql_fetch_assoc($result);
-			if (password_verify($password,$row['Password'])){ 
-        		echo "Login Successful";
-			} else {
-				echo "Login Unsuccesful! 0_0\n";
-				echo $row['Password'];
-				echo "\n" . $password;
+			if (password_verify($password,$row['Password'])){
+				generateTitle("Logging in...", "../index/optionpage.php", "1"); 
+			} else {	
+				generateTitle("Logging in...", "../index/homepage.php?error=true", "1");
 			}
+		} else {
+			generateTitle("Logging in...", "../index/homepage.php?error=true", "1");
 		}
-	generateHeader();
 	echo "Logging in please wait...<br>";
 	echo "<a href='../index/homepage.php'>If nothing happens after 5 seconds, click here to go back.</a>";
 	generateFoot();
