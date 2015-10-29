091f6548d025fba030746932a6d460073d87667a
Danny <danny.oldenburger@gmail.com>
2015-10-29 15:10:46 +0100

    Small fix to imageprovider.php

--- Shoppingcart/index.php
+++ Shoppingcart/index.php
@@ -97,7 +97,7 @@ if(isset($_SESSION["cart_item"])){
 	?>
 		<div class="product-item">
 			<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["Product_number"]; ?>">
-			<div class="product-image">"<?php getImgTag($product_array[$key]["Product_number"],128); ?>"</div>
+			<div class="product-image"><?php echo getImgTag($product_array[$key]["Product_number"],128, 100); ?></div>
 			<div><strong><?php echo $product_array[$key]["Product_name"]; ?></strong></div>
 			<div class="product-price"><?php echo "$".$product_array[$key]["Price"]; ?></div>
 			<div><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Add to cart" class="btnAddAction" /></div>
