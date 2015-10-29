06a52adacb37f862859abc326bace8b8f26bc0aa
Hanze Hogeschool <joostholwerda@hotmail.com>
2015-10-29 12:19:43 +0100

    .

--- Product overview TEST/Test3.php
+++ Product overview TEST/Test3.php
@@ -9,18 +9,15 @@ mysqli_select_db($link,"projectdb")
 $klantnummer = "50";
 
 $create_query =	"	
-					CREATE TABLE IF NOT EXISTS `Shoppingcart` (	
-					`Product_number` int(10) NOT NULL AUTO_INCREMENT, 
-				  	`Product_name` varchar(254) NOT NULL,
-				  	`Discription` varchar(254) NOT NULL,
-					`Price` varchar(254) NOT NULL,
-					`In_stock` varchar(254) NOT NULL,
-					`Catagory` varchar(254) NOT NULL,
-					PRIMARY KEY (`Product_name`)		
-					) 
-					ENGINE=InnoDB  
-					DEFAULT CHARSET=utf8_unicode_ci
-					AUTO_INCREMENT=7; 
+				CREATE TABLE IF NOT EXISTS  `Shoppingcart` (
+				 			`Product_number` INT( 10 ) NOT NULL AUTO_INCREMENT ,
+							`Product_name` VARCHAR( 254 ) NOT NULL ,
+						 	`Discription` VARCHAR( 254 ) NOT NULL ,
+							`Price` VARCHAR( 254 ) NOT NULL ,
+							`In_stock` VARCHAR( 254 ) NOT NULL ,
+							`Catagory` VARCHAR( 254 ) NOT NULL ,
+							PRIMARY KEY (  `Product_number` )
+				)
 				";
  
 $insert_query = "
