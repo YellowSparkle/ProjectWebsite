070aba94d814196d6675fefbb34cedc63fec540f
Hanze Hogeschool <xavier.kerkmeester@gmail.com>
2015-11-02 12:30:14 +0100

    Option page css fix

--- index/optionpage.php
+++ index/optionpage.php
@@ -3,37 +3,24 @@
 <?php
 require_once "../utility/HTMLGenerator.php";
 require_once '../utility/Header.php';
+generateTitle("Make your choice.");
 generateHeader();
  ?>
 
 
-<html>
-	<head>
-		<title>Make your choice.</title>
-	</head>
-	
-
-	<link rel="stylesheet" type="text/css" href="../utility/stylesheet.css">
-	
-<body>
+<link rel="stylesheet" type="text/css" href="../utility/assets/css/main.css">
 
  
  <div id="buttonBox2">
-		<form id="categoryButtons" class="container" action="../sell/category.php" method="get">
-			<div class=sell_left>
-				<p><img src="../img/sellimage.jpg" width="400px" height="350px" alt="" border=2></p>
-				<p><input class="button special" type="submit" name="submit" value="Sell a Product">
-			</div>
-			<div class=buy_right>
-				<p><img src="../img/buyimage.jpg" width="400px" height="350px" alt="" border=2></p>
-				<input class="button special" type="submit" name="submit" value="Buy a Product">
-				</div>
-		</form>
-	</div> 
+ 	<form id="categoryButtons" class="container" action="../sell/category.php" method="get">
+ 		<div class=sell_left><img class=optionimage src="../img/sellimage.jpg" width="400px" height="350px" alt="" border=2></div>
+ 		<div class =buttonleft><input class="button special" type="submit" name="submit" value="Sell a Product"></div>
+ 		<div class=buy_right><img class=optionimage src="../img/buyimage.jpg" width="400px" height="350px" alt="" border=2></div>
+ 		<div class =buttonright><input class="button special" type="submit" name="submit" value="Buy a Product"></div>
+ 	</form>
+ </div> 
 
-   
-</body>
-</html>
+
 
 <?php
 //knoppen opmaak
