756b94923d395a045ad40144c429f28fa06cf99a
Hanze Hogeschool <onzin09@gmail.com>
2015-11-04 14:57:49 +0100

    product toevoegen werkt nu, alleen nog niet gelinkt

--- Registreren_pagina/Product_toevoegen_database_page.php
+++ Registreren_pagina/Product_toevoegen_database_page.php
@@ -9,31 +9,37 @@
 	
 	
 	<div class="features">
-	<form name="registerform" method="post" action="Register_page.php" class="tekst">
+	<form name="add_product" method="post" action="Product_toevoegen_database_script.php" class="tekst">
 	<ul>
 		
-		<li>First name</li>
+		<li>Product name</li>
 		<li><input name="product_name" type="text" size="50" value=""></li>
 		
-		<li>Insertion</li>
+		<li>Description</li>
 		<li><input name="description" type="text" size="50" value=""></li>
 			
-		<li>*Last name</li>
+		<li>Price</li>
 		<li><input name="price" type="text" size="50" value=""></li>
 			
 		<li>In stock</li>
 		<li><input name="in_stock" type="text" size="50" value=""></li>
 			
 		<li>Category</li>
-		<input type="radio" name="category" value="Audio"> Audio
-		<input type="radio" name="category" value="Computers"> Computers
-		<input type="radio" name="category" value="Phones"> Phones
-		<input type="radio" name="category" value="Radio"> Radio
-		<input type="radio" name="category" value="Television"> Television
+		<input type="radio" name="category" id="Audio" value="Audio">
+		<label for="Audio">Audio</label>
+		
+		<input type="radio" name="category" id="Computers" value="Computers">	
+		<label for="Computers">Computers</label>
+						
+		<input type="radio" name="category" id="Phones" value="Phones">
+		<label for="Phones">Phone</label>		
+
+		<input type="radio" name="category" id="Radio" value="Radio">
+		<label for="Radio">Radio</label>		
+
+		<input type="radio" name="category" id="Television" value="Television">
+		<label for="Television">Television</label>
 		
 		<li><input type="reset" value="Reset"> <input type="submit" name="versturen" value="Add to database"></li>
-		<p>
-			All fields with * must be filled in correctly.
-		</p>
 	</ul>
 	</form></div>
