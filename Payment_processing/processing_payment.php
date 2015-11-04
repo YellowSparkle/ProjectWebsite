19cd76dc1469ab697045fac76ffdc3fb1187c974
Hanze Hogeschool <onzin09@gmail.com>
2015-11-04 11:55:03 +0100

    sourcecode opmaak aangepast

--- Payment_processing/processing_payment.php
+++ Payment_processing/processing_payment.php
@@ -1,23 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>processing</title>
-	</head>
-	<body>
+<html>
 	
-<?php
-if(isset($_POST["submit"]))
-	{
+<head>
+	<title>processing</title>
+</head>
+
+
+<body>
+
+<meta http-equiv="refresh" content="0;url=payment_complete.php" />
+
+</body>
+
+</html>
+
+<!--<?php
+	if(isset($_POST["submit"])) {
 		$username = isset($_POST["username"]) ? $_POST["username"] : "Default";
 		$password = isset($_POST["password"]) ? $_POST["password"] : "Default";
 		echo"";?> <meta http-equiv="refresh" content="0;url=payment_complete.php" /><?php 
-	}
-else 
-	{
+	} else {
 		echo "You did not submit the form, please do not type just the url, and instead login via the bank tab";
 	}
- ?>
-	</body>
-	
-</html>
-
+?>-->
